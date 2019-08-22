06ad91a2246cbef3aed36bf257feca9ba1995c1d
Aarni Koskela <akx@iki.fi>
2019-08-22 09:30:41 +0300

    2to3, emblacken

--- license_grep.py
+++ license_grep.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function, unicode_literals
+
 import sys
 
 from fnmatch import fnmatch
@@ -11,8 +11,14 @@ import subprocess
 
 
 EXCLUDE_PATTERNS = [
-    'contrib', 'docs', 'tests*', 'node_modules',
-    'bower_components', 'var', '__pycache__', 'LC_MESSAGES',
+    'contrib',
+    'docs',
+    'tests*',
+    'node_modules',
+    'bower_components',
+    'var',
+    '__pycache__',
+    'LC_MESSAGES',
     'venv*',
 ]
 
@@ -30,11 +36,14 @@ def process_dist(dist):
         metadata = dist.get_metadata('METADATA')
     if metadata:
         license = find_license(metadata)
-    return {"%s@%s" % (dist.project_name, dist.version): {
-        "via": "python",
-        "version": dist.version,
-        "license": license or "<no license found>"
-    }}
+    return {
+        "%s@%s"
+        % (dist.project_name, dist.version): {
+            "via": "python",
+            "version": dist.version,
+            "license": license or "<no license found>",
+        }
+    }
 
 
 def find_license(metadata):
@@ -61,7 +70,7 @@ def process_node_manifest(node_manifest):
     dirname = os.path.realpath(os.path.dirname(node_manifest))
 
     if node_manifest.endswith("package.json"):
-        cmd = "npm-license -e json"
+        cmd = "license-checker -e json"
         via = "npm"
     elif node_manifest.endswith("bower.json"):
         cmd = "bower-license -e json"
@@ -70,14 +79,11 @@ def process_node_manifest(node_manifest):
         raise NotImplementedError("Not implemented")
 
     json_data, _ = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        shell=True,
-        cwd=dirname
+        cmd, stdout=subprocess.PIPE, shell=True, cwd=dirname
     ).communicate()
 
     data = json.loads(json_data.decode("UTF-8"))
-    for item in data.values():
+    for item in list(data.values()):
         item["via"] = via
     return data
 
@@ -104,7 +110,7 @@ def write_license_table(data, fp):
 
 def strip_versions(data):
     out_data = {}
-    for key, value in sorted(data.items(), reverse=True):
+    for key, value in sorted(list(data.items()), reverse=True):
         key = key.split("@")[0]
         out_data[key] = value
     return out_data
@@ -126,8 +132,6 @@ def main():
     if args.strip_versions:
         data = strip_versions(data)
         print("%d packages after stripping versions." % len(data), file=sys.stderr)
-
-
 
     if args.write_json:
         json.dump(data, args.write_json, sort_keys=True)
